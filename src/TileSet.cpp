f82a37f642c5a05d34dc2001e8184f452a489aeb
ThalissonMelo <tallisonmelo46@gmail.com>
2019-04-10 16:46:56 -0300

    Making final changes on T3

--- src/TileSet.cpp
+++ src/TileSet.cpp
@@ -9,20 +9,17 @@
       this->columns = tileSet.getWidth()/tileWidth;
       this->rows = tileSet.getHeight()/tileHeight;
     }
-    // void TileSet::RenderTile(unsigned index, float x, float y){
-    //   if(index < this->rows*this->columns){
-    //     int x_axis = this->tileWidth*(index%this->columns);
-    //     int y_axis = this->tileHeight*(index%this->rows);
-    //     tileSet.setClip(x_axis, y_axis, this->tileWidth, this->tileHeight);
-    //     tileSet.Render(x, y);
-    //   }
-    // }
-    void TileSet::RenderTile(unsigned index, float x, float y) {
-	if(index < rows*columns) {
-		tileSet.setClip(tileWidth*(index%columns), tileHeight*(index/columns), tileWidth, tileHeight);
-		tileSet.Render(x, y);
-	}
-}
+
+    void TileSet::RenderTile(unsigned index, float x, float y){
+    
+      if(index < this->rows*this->columns){
+        int x_axis = this->tileWidth*(index%this->columns);
+        int y_axis = this->tileHeight*(index/this->columns);
+        tileSet.setClip(x_axis, y_axis, this->tileWidth, this->tileHeight);
+        tileSet.Render(x, y);
+      }
+    
+    }
 
     int TileSet::GetTileWidth(){
       return this->tileWidth;
